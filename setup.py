e406d22017364e4024dd37b961233ecf77410673
Lyuwen Fu <lyuwenfu@zhejianglab.edu.cn>
2022-09-07 13:52:55 +0800

    update build configs

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python3
+""" LPlot is a generic command line plotting tool written in Python
+
+- It provides a generic backend that can be interfaced with multiple
+  plotting engines. Currently matplotlib is supported.
+- It also supports saving and loading configurations into/from a file,
+  making repeated plotting much easier.
+"""
+DOCLINES = (__doc__ or '').split("\n")
+
 from setuptools import setup
 
 # Using the versioneer to set the version of the distribution
@@ -9,11 +19,9 @@ if __name__ == '__main__':
   setup(name=name,
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    description='This is the {} module.'.format(name),
     author='Lyuwen Fu',
-    #  packages=packages,
+    description=DOCLINES[0],
+    long_description="\n".join(DOCLINES[2:]),
     requires=['numpy', 'PyYAML', 'matplotlib'],
     provides=['lplot'],
-    #  scripts=scripts,
-    #  include_package_data=True,
     )
