4251fbb163f42b14b4f2f576a4cc4de54ea9f1de
lyuwen <lyuwen@users.noreply.github.com>
2022-09-07 04:54:53 +0000

    deploy: 60e72eed6de1c1c5a12a4831de1e43b0ae485d35

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.gcfaa200',
+    VERSION: '0+untagged.1.g60e72ee',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
