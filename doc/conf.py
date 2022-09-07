460fbe75dc129c4a4a78ee74e5be8078d1bd273f
Lyuwen Fu <lyuwenfu@zhejianglab.edu.cn>
2022-09-07 09:55:07 +0800

    add version info in sphinx conf file

--- doc/conf.py
+++ doc/conf.py
@@ -3,12 +3,27 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), os.path.pardir))
+
+import lplot
+
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'LPlot'
 copyright = '2022, Lyuwen Fu'
 author = 'Lyuwen Fu'
+release = lplot.__version__
+version = lplot.__version__
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
